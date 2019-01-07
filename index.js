b271678854640461329bd268b696dc878344b566
Mat Gilbert <matgilbert@gmail.com>
2019-01-07 14:56:27 -0500

    Collecting pairs, added scripts for testing validation

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ console.log('Pairs Detection');
 //console.log(process.argv);
 
 let input = process.argv[2];
+let rawValues = input.split(',');
 
-if(!input){
-    console.error('No input provided, please pass a string of integers between 1 and 9 surrounded by single quotes');
+if(!input || rawValues.length < 1){
+    console.error('ERROR: No input provided, please pass a string of integers between 1 and 9 surrounded by single quotes');
     process.exit();
 }
 
-console.log(`Input: ${input}`);
+let integers = rawValues.map( value => {
+    
+    let integer = new Number(value);
+    
+    if(isNaN(integer) || integer > 9 || integer < 0){
+        console.error('ERROR: Each segement of the string must be an integer 1-9. No decimals allowed.')
+    }
+
+    return integer
+})
+
+let pairs = [];
+
+//Iterate over all of the values in the array
+integers.forEach((int, index) => {
+    
+    integers.forEach((secondInt, secondIndex) => {
+
+        if(index != secondIndex){
+            //console.log(int, secondInt);
+            //return [int, secondInt];
+            pairs.push([int, secondInt])
+        } 
+
+    })
+
+})
+
+console.log(`Input: ${integers}`);
+console.log(`Pairs: ${pairs.join(" | ")}`);
+
